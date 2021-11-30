e5191be3de999523db8e1e47c534bd8b4ad9e9ed
goncalo pinto <gcorreiapinto@gmail.com>
2021-11-30 12:41:22 +0000

    Initial commit

---
+++ config/webpack/environment.js
@@ -0,0 +1,6 @@
+const { environment } = require('@rails/webpacker')
+
+const webpack = require('webpack');
+// Preventing Babel from transpiling NodeModules packages
+environment.loaders.delete('nodeModules');
+module.exports = environment
