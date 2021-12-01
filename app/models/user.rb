085d9736186cec5ef6be9d3c69e72809ec62a93d
Wellington Oliveira <wellington.calor@gmail.com>
2021-12-01 18:38:16 +0000

    table changes to game_session

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2, :discord]
 
-  has_many :sessions
+  has_many :game_sessions
   has_many :lobbies, foreign_key: :owner_id
 end
 
--- db/migrate/20211130151744_create_game_sessions.rb
+++ db/migrate/20211130151744_create_game_sessions.rb
@@ -1,6 +1,6 @@
 class CreateGameSessions < ActiveRecord::Migration[6.1]
   def change
-    create_table :sessions do |t|
+    create_table :game_sessions do |t|
       t.references :game, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
       t.datetime :started_at
