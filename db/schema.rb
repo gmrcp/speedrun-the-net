01d3d334222e2d7a312099e5356e7a4658e37f75
Wellington Oliveira <wellington.calor@gmail.com>
2021-12-01 18:40:28 +0000

    updated game sessions controller

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,19 @@ ActiveRecord::Schema.define(version: 2021_12_01_113034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "game_sessions", force: :cascade do |t|
+    t.bigint "game_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "started_at"
+    t.datetime "ended_at"
+    t.string "clicks", default: [], array: true
+    t.boolean "ready?", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_game_sessions_on_game_id"
+    t.index ["user_id"], name: "index_game_sessions_on_user_id"
+  end
+
   create_table "games", force: :cascade do |t|
     t.bigint "lobby_id", null: false
     t.string "category", default: "default"
@@ -45,19 +58,6 @@ ActiveRecord::Schema.define(version: 2021_12_01_113034) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
-  create_table "sessions", force: :cascade do |t|
-    t.bigint "game_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "started_at"
-    t.datetime "ended_at"
-    t.string "clicks", default: [], array: true
-    t.boolean "ready?", default: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["game_id"], name: "index_sessions_on_game_id"
-    t.index ["user_id"], name: "index_sessions_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -72,10 +72,10 @@ ActiveRecord::Schema.define(version: 2021_12_01_113034) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "game_sessions", "games"
+  add_foreign_key "game_sessions", "users"
   add_foreign_key "games", "lobbies"
   add_foreign_key "lobbies", "users", column: "owner_id"
   add_foreign_key "messages", "lobbies"
   add_foreign_key "messages", "users"
-  add_foreign_key "sessions", "games"
-  add_foreign_key "sessions", "users"
 end
