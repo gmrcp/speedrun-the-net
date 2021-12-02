aa4721207ea178bf2eccbbe1224ab2ad0e4fa304
Sérgio Assis Cerqueira <jovis.sergio@gmail.com>
2021-12-02 16:31:46 +0000

    home page semi-responsive

--- test/controllers/game_sessions_controller_test.rb
+++ test/controllers/game_sessions_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class SessionsControllerTest < ActionDispatch::IntegrationTest
+class GameSessionsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
