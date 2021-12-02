27b44b57ad058bb94afb17853211398b8b9e3922
goncalo pinto <gcorreiapinto@gmail.com>
2021-12-02 19:58:55 +0000

    changed js border-radius
    Signed-off-by: goncalo pinto <gcorreiapinto@gmail.com>

--- app/javascript/components/change_main_container_height.js
+++ app/javascript/components/change_main_container_height.js
@@ -1,6 +1,7 @@
 const changeMainContainerHeight = () => {
   const main_overlay = document.getElementById('main-overlay')
   main_overlay.style.height = '85vh'
+  main_overlay.style.borderRadius = '40px 20px 20px 40px'
 }
 
 export { changeMainContainerHeight }
