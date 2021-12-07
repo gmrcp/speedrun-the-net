386554bd0a0862e49857554396db3902712f2899
goncalo pinto <gcorreiapinto@gmail.com>
2021-12-07 14:16:03 +0000

    preparing PR
    Signed-off-by: goncalo pinto <gcorreiapinto@gmail.com>

--- app/javascript/components/change_main_container_height.js
+++ app/javascript/components/change_main_container_height.js
@@ -1,7 +1,6 @@
 const changeMainContainerHeight = () => {
-  const main_overlay = document.getElementById('main-overlay')
-  main_overlay.style.height = '85vh'
-  main_overlay.style.borderRadius = '40px 20px 20px 40px'
+  const main_overlay = document.getElementById('main-overlay');
+  main_overlay.classList.add('change-height-on-play');
 }
 
 export { changeMainContainerHeight }
