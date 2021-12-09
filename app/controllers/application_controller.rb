2579a703a536701b7a7bebf2fb442c3e9c74a406
James Donck <james.donck@gmail.com>
2021-12-09 13:08:31 +0000

    Implementing the title, metatags, cover,...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,4 +22,8 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
+
+  def default_url_options
+    { host: ENV["http://www.speedrunthe.net/"] || "localhost:3000" }
+  end
 end
